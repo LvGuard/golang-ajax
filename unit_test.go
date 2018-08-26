675e8347ed1c6be3f3f333528a92f2b15ad306ef
LvGuard <42619440+LvGuard@users.noreply.github.com>
2018-08-26 09:29:58 +0300

    Add files via upload
    Added extra test cases

--- unit_test.go
+++ unit_test.go
@@ -147,49 +147,93 @@ func TestInsertProcess(t *testing.T) {
 func TestDataTableLoad(t *testing.T) {
     tables := []struct {
         r bool
-        v1 Context
-        v2 Customer
+        v Context
     }{
         {true, Context{
+                    SearchBy:          "[123] 'Specific' First Name -- for test purposes",
+                    OrderByField:      "99",
+                    OrderByDirection:  "invalid direction asc",
+                    CurrentPage:       0,
+                    Language:          "1",
+        }},
+        {true, Context{
+                    SearchBy:          "(не-'латинские' символы) [144] Very Specific LAST Name -- for test purposes",
+                    OrderByField:      "3",
+                    OrderByDirection:  "desc",
+                    CurrentPage:       0,
+                    Language:          "3",
+        }},
+        {true, Context{
+                    SearchBy:          "1992-10-18",
+                    OrderByField:      "4",
+                    OrderByDirection:  "asc",
+                    CurrentPage:       0,
+                    Language:          "1",
+        }},
+        {true, Context{
+                    SearchBy:          "Female",
+                    OrderByField:      "5",
+                    OrderByDirection:  "desc",
+                    CurrentPage:       0,
+                    Language:          "2",
+        }},
+        {true, Context{
                     SearchBy:          "First_Name.Last_Name@longdummytestemailaddressforgolangunittests.go",
+                    OrderByField:      "6",
+                    OrderByDirection:  "asc",
+                    CurrentPage:       0,
+                    Language:          "1",
+        }},
+        {true, Context{
+                    SearchBy:          "[-=+] 'Test' address in test city with digits 12974527 72628 98271",
+                    OrderByField:      "7",
+                    OrderByDirection:  "asc",
+                    CurrentPage:       0,
+                    Language:          "2",
+        }},
+        {false, Context{
+                    SearchBy:          "no such combintion for tests in test sample",
                     OrderByField:      "1",
                     OrderByDirection:  "asc",
                     CurrentPage:       0,
                     Language:          "1",
-        },     Customer{
-                    FirstName:         "First Name",
-                    LastName:          "Last_Name",
-                    BirthDate:         "1995-09-01",
-                    Gender:            "Male",
-                    Email:             "First_Name.Last_Name@longdummytestemailaddressforgolangunittests.go",
-                    Address:           "",
         }},
     }
+    c := Customer{
+                    FirstName:         "[123] 'Specific' First Name -- for test purposes",
+                    LastName:          "(не-'латинские' символы) [144] Very Specific LAST Name -- for test purposes",
+                    BirthDate:         "1992-10-18",
+                    Gender:            "Female",
+                    Email:             "First_Name.Last_Name@longdummytestemailaddressforgolangunittests.go",
+                    Address:           "[-=+] 'Test' address in test city with digits 12974527 72628 98271",
+    }
     InitDb()
+    //Preparations
+    if (!InsertProcess(&c, "1") ) {
+        t.Errorf("Preparation1: InsertProcess failed: %v", c)
+    }
+    fmt.Println("Preparation1: Inserted record with cid",c.Cid)
+    //Testing
     for _, table := range tables {
-        if (!InsertProcess(&table.v2, table.v1.Language) ) {
-            t.Errorf("Preparation1: InsertProcess failed: %v", table.v2)
-        }
-        fmt.Println("Preparation1: Inserted record with cid",table.v2.Cid)
-        
         var dataTable DataTable
-        DataTableLoad(&table.v1, &dataTable)
+        DataTableLoad(&table.v, &dataTable)
 
         var found bool
-        for _, c := range dataTable.Customers {
-            if ( (c.Email == table.v2.Email) && (c.Cid == table.v2.Cid)) {
+        for _, c1 := range dataTable.Customers {
+            if ( (c1.Cid == c.Cid)&&(c1.FirstName == c.FirstName)&&(c1.LastName == c.LastName)&&(c1.BirthDate == c.BirthDate)&&(c1.Gender == c.Gender)&&(c1.Email == c.Email)&&(c1.Address == c.Address) ) {
                 found = true
             }
         }
-        if (!found) {
-            t.Errorf("Test failed as %v not found within %v", table.v2, dataTable)
+        if (found != table.r) {
+            t.Errorf("Test failed as (%v!=%v): %v (%v)found within %v", found, table.r, c, found, dataTable)
         }
 
-        r, err := DeleteProcess(table.v2.Cid)
-        if (!r) {
-            t.Errorf("DeleteProcess failed to delete Cid %v (%v). Full struct %v", table.v2.Cid, err, table.v2)
-        }
-        fmt.Println("Deleted record with cid",table.v2.Cid)
     }
+    //Clean-Up
+    r, err := DeleteProcess(c.Cid)
+    if (!r) {
+        t.Errorf("DeleteProcess failed to delete Cid %v (%v). Full struct %v", c.Cid, err, c)
+    }
+    fmt.Println("Deleted record with cid",c.Cid)
     db.Close()
 }
